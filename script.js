2413d0fa0cae8fa9656111c155e0f6c067d91295
BenjiLuaba <benjiluaba@gmail.com>
2025-04-25 03:14:31 +0100

    Ma première version

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    displayProjects();
-    document.getElementById("downloadCv").addEventListener("click", function(event) {
-        event.preventDefault();
-        const fileUrl = this.href;
-        window.open(fileUrl, "_blank");
-    });
-});
+//Télécharger le CV dans un nouvel onglet
+document.querySelector('.btn-cv').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.open('/assets/CV_2025-02-25_Benjamin_LUABA.pdf', '_blank'); 
+  });
+  
+  //Confirmation formulaire de contact
+  document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert("Merci pour votre message ! Je vous répondrai bientôt 😊");
+    this.reset();
+  });
+  
